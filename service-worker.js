dd7a86299f9e778916eca035e00971142eaf490a
Hendrik de Graaf <hendrik@dhis2.org>
2020-09-01 13:05:40 +0000

    6b75ae3 chore: remove css-reset which is provided by the platform

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.21d48e109982913a997225d465302b87.js"
+  "./precache-manifest.415b959d76eac6e8b2bff87155b5d963.js"
 );
 
 self.addEventListener('message', (event) => {
