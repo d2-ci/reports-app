61cce4c66c9d295cb14090eb97725e3d9b11aa2d
GitHub <noreply@github.com>
2020-09-08 12:58:24 +0000

    1f20bf8 fix: add dhis2 period utility (#164)

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.80ef84c67831f0f170c282099a81eed2.js"
+  "./precache-manifest.47fdc6fed57f1de323b6c815921f9742.js"
 );
 
 self.addEventListener('message', (event) => {
